60eb58885a83638a115ce3982ec387e1ef593270
gilcier <gilcier@itarget.com.br>
2013-12-23 18:00:42 -0300

    alterações modulo contatos

--- module/Core/config/module.config.php
+++ module/Core/config/module.config.php
@@ -1,3 +1,13 @@
 <?php
+//function getProperty($property){
+//        return method_exists($this, 'get'. $property)? call_user_method('get'. $property, $this) : $this->{strtolower($property)};
+//    }
+//
+//    function getObjProperties($obj){
+//        $vars = get_class_vars(get_class($obj));
+//        foreach($vars as $var) {$arr[$var] = $obj->getProperty($var)}
+//        return arr;
+//    }
+
 return array(
 );
